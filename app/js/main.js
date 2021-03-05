64dced1f4aac4290e99da1694cf37a17aec564a9
Shifu <kris.deniso@mail.ru>
2021-03-05 20:10:04 +0300

    added reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  
+  let swiper = new Swiper('.reviews__slider', {
+    navigation: {
+      nextEl: '.reviews__arrow--next',
+      prevEl: '.reviews__arrow--prev',
+    },
+  });
 });
