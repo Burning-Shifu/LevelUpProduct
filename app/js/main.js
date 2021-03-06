a1c1533387f4db85bd7e47876cbbe2262fc5e376
Shifu <kris.deniso@mail.ru>
2021-03-06 17:38:06 +0300

    added order & footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,50 @@ document.addEventListener('DOMContentLoaded', () => {
       prevEl: '.reviews__arrow--prev',
     },
   });
+
+
+  // timer
+  
+  const endDate = Date.now() + (30 * 60 * 1000);
+  
+  function getTimeRemaining() {
+    const t = endDate - Date.now(),
+          mins = Math.floor((t / 1000 / 60) % 60),
+          secs = Math.floor((t / 1000) % 60);
+          
+    return {
+      'total': t,
+      'mins': mins,
+      'secs': secs
+    }
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  
+  function setTimeRemaining() {
+    const mins = document.querySelector('#minutes'),
+          secs = document.querySelector('#seconds'),
+          timeInterval = setInterval(updTimer, 1000);
+
+    updTimer();
+
+    function updTimer() {
+      const t = getTimeRemaining();
+
+      mins.textContent = getZero(t.mins);
+      secs.textContent = getZero(t.secs);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  
+  setTimeRemaining(); 
 });
